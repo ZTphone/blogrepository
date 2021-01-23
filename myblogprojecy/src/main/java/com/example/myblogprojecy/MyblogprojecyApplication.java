c323cac7f916d2dde143a87839123092cebc0bca
MYUN <1490999740@qq.com>
2021-01-24 02:25:43 +0800

    first commit

---
+++ myblogprojecy/src/main/java/com/example/myblogprojecy/MyblogprojecyApplication.java
@@ -0,0 +1,13 @@
+package com.example.myblogprojecy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogprojecyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogprojecyApplication.class, args);
+    }
+
+}
---
+++ myblogprojecy/src/test/java/com/example/myblogprojecy/MyblogprojecyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myblogprojecy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogprojecyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
